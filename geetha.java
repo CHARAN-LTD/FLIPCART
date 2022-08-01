029fe4c36be7e5a68389e21a163fa316033c867c
Charan858 <csri14858@gmail.com>
2022-08-01 20:44:22 +0530

    commited in status for 2nd time

--- geetha.java
+++ geetha.java
@@ -2,4 +2,4 @@ Hii, welcome to geetha arts
 today is monday
 and holiday
 tomarrow is tuesday
-
+afterthat is wed
--- uv.java
+++ uv.java
@@ -1,10 +1,9 @@
 updated line
 Hii welcome to UV creations
-<<<<<<< HEAD
 pramod
-=======
 today we are releasing RADHESHYAM
 people will love it
->>>>>>> 5937cc8e06e610beab33f0e3d3e4af820eab499f
 tomarrow is tuesday
+after that is wednesday
+
 
